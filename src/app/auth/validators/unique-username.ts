6f42f0527c186c142561032544e5848ba23bc467
SJCMN <sam.clavette@gmail.com>
2022-03-16 17:12:40 -0500

    add auth http service

--- src/app/auth/validators/unique-username.ts
+++ src/app/auth/validators/unique-username.ts
@@ -1,19 +1,17 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+
 import { AbstractControl, AsyncValidator } from '@angular/forms';
 import { catchError, map, of } from 'rxjs';
+import { AuthService } from '../auth.service';
 
 @Injectable({ providedIn: 'root' })
 export class UniqueUsername implements AsyncValidator {
-  constructor(private http: HttpClient) {}
+  constructor(private authService: AuthService) {}
 
-  validate = (control: AbstractControl): any => {
+  validate = (control: AbstractControl) => {
     const { value } = control;
 
-    return this.http
-      .post<any>('https://api.angular-email.com/auth/username', {
-        username: value,
-      })
+    return this.authService.usernameAvailable(value)
       .pipe(
         map(() => {
           return null;
