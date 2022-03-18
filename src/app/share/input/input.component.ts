146b584c885529bb29d68b0d1539e7c7786811ab
SJCMN <sam.clavette@gmail.com>
2022-03-18 15:40:23 -0500

    add input component

--- src/app/share/input/input.component.ts
+++ src/app/share/input/input.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , Input} from '@angular/core';
 
 @Component({
   selector: 'app-input',
@@ -7,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InputComponent implements OnInit {
 
-  constructor() { }
+@Input() label?: string ;
+@Input() control: any;
+
+  constructor() { 
+    
+  }
 
   ngOnInit(): void {
   }
