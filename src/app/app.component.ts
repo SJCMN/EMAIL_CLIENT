38ec80f6b7402f25930af1087044fb6631989e06
SJCMN <sam.clavette@gmail.com>
2022-03-24 10:59:16 -0500

    add routerLinkActive and routerLinkActiveOptions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from './auth/auth.service';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,12 +8,11 @@ import { AuthService } from './auth/auth.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  signedin: boolean = false;
-  constructor(private authService: AuthService){}
+  signedin$: BehaviorSubject<boolean>;
 
-  ngOnInit(){
-    this.authService.signedin$.subscribe((signedin)=>{
-      this.signedin = signedin
-    })
+  constructor(private authService: AuthService){
+    this.signedin$ = this.authService.signedin$
   }
+
+
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, pipe } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
 interface UsernameAvailableResponse {
