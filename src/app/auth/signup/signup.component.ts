2d53231d59b3b10a63e6442ba96cd8fab15b8e83
SJCMN <sam.clavette@gmail.com>
2022-03-24 16:54:54 -0500

    adding sign in routes - bug with navigateByUrl()

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -3,7 +3,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatchPassword } from '../validators/match-password';
 import { UniqueUsername } from '../validators/unique-username';
 import { AuthService } from '../auth.service';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -41,7 +41,8 @@ export class SignupComponent implements OnInit {
   constructor(
     private matchPassword: MatchPassword,
     private uniqueUsername: UniqueUsername,
-    private authService: AuthService
+    private authService: AuthService,
+    private router: Router
     ) { }
 
   ngOnInit(): void {
@@ -54,6 +55,7 @@ export class SignupComponent implements OnInit {
     this.authService.signup(this.authForm.value)
     .subscribe({next: response => {
       // navigate to another route
+      this.router.navigateByUrl('/inbox')
     },
   
     error: (err) => {
