2b63628f9a3fb5897840dc57e676bc056e3e0b9c
SJCMN <sam.clavette@gmail.com>
2022-03-29 12:43:41 -0500

    updating input labels

--- src/app/inbox/inbox.module.ts
+++ src/app/inbox/inbox.module.ts
@@ -4,16 +4,8 @@ import { CommonModule } from '@angular/common';
 import { InboxRoutingModule } from './inbox-routing.module';
 import { HomeInboxComponent } from './home-inbox/home-inbox.component';
 
-
-
-
 @NgModule({
-  declarations: [
-    HomeInboxComponent
-  ],
-  imports: [
-    CommonModule,
-    InboxRoutingModule
-  ]
+  declarations: [HomeInboxComponent],
+  imports: [CommonModule, InboxRoutingModule],
 })
-export class InboxModule { }
+export class InboxModule {}
