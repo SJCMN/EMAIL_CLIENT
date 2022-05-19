b3ed269e69191a26ba35911aac35de1cb37a5b9a
SJCMN <sam.clavette@gmail.com>
2022-05-19 11:05:39 -0500

    email components -wq exit quit

--- src/app/inbox/inbox.module.ts
+++ src/app/inbox/inbox.module.ts
@@ -3,9 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { InboxRoutingModule } from './inbox-routing.module';
 import { HomeInboxComponent } from './home-inbox/home-inbox.component';
+import { EmailShowComponent } from './email-show/email-show.component';
+import { EmailCreateComponent } from './email-create/email-create.component';
+import { EmailReplyComponent } from './email-reply/email-reply.component';
+import { EmailIndexComponent } from './email-index/email-index.component';
 
 @NgModule({
-  declarations: [HomeInboxComponent],
+  declarations: [HomeInboxComponent, EmailShowComponent, EmailCreateComponent, EmailReplyComponent, EmailIndexComponent],
   imports: [CommonModule, InboxRoutingModule],
 })
 export class InboxModule {}
